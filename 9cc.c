a258e4e21a6df2b96bf6bf7f36d22c9bed5bf4c6
MizushimaToshihiko <pandachan5228@gmail.com>
2021-09-30 02:54:16 +0000

    step3 in the middle

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,35 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// types of token
+typedef enum {
+  TK_RESERVED,  // symbol
+  TK_NUM,       // integer
+  TK_EOF,       // end of the input
+} TokeKind;
+
+typedef struct Token Token;
+
+struct Token {
+  TokenKind kind;
+  Token *next;
+  int val;
+  char *str;
+};
+
+// the global variableof the current token
+Token *token;
+
+void error (char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, "\n");
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
