c9b945e837c5ce86aba922bda9eeae9683f367aa
MizushimaToshihiko <pandachan5228@gmail.com>
2021-12-27 15:13:53 +0900

    add the test function for typename reading

--- main_test.go
+++ main_test.go
@@ -43,3 +43,34 @@ func TestCompile(t *testing.T) {
 	}
 	t.Logf("\noutput:\n%s", string(b))
 }
+
+func TestGetTypeName(t *testing.T) {
+	cases := map[string]struct {
+		in   string
+		want string
+	}{
+		"case1": {"[2]int", "[2]int"},
+		"case2": {"[2][3]int", "[2][3]int"},
+		"case3": {"****int", "pointer"},
+		"case4": {"byte", "byte"},
+		"case5": {"string", "string"},
+		"case6": {"*string", "pointer"},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			userInput = append([]rune(c.in), 0)
+			curIdx = 0
+			var err error
+			token, err = tokenize()
+			if err != nil {
+				t.Fatal(err)
+			}
+			ty := readTypePreffix()
+
+			if ty.Name != c.want {
+				t.Fatalf("%s expected, but got %s", c.want, ty.Name)
+			}
+		})
+	}
+}
