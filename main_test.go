bf95f888d55e919ea35c25cb2f18b471f8741956
MizushimaToshihiko <pandachan5228@gmail.com>
2021-12-29 08:16:14 +0000

    deleted unnecessary test function

--- main_test.go
+++ main_test.go
@@ -1,48 +1,8 @@
 package main
 
 import (
-	"os"
-	"os/exec"
 	"testing"
 )
-
-func TestCompile(t *testing.T) {
-
-	var asm *os.File
-	var err error
-	asm, err = os.Create("testdata/asm.s")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := asm.Close(); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	if err = compile("testdata/tests.go", asm); err != nil {
-		t.Fatal(err)
-	}
-
-	var b []byte
-	b, err = exec.Command(
-		"gcc",
-		"-static",
-		"-g",
-		"-o",
-		"testdata/asm",
-		"testdata/asm.s",
-	).CombinedOutput()
-	if err != nil {
-		t.Fatalf("\noutput:\n%s\n%v", string(b), err)
-	}
-
-	b, err = exec.Command("testdata/asm").CombinedOutput()
-	if err != nil {
-		t.Fatalf("\noutput:\n%s\n%v", string(b), err)
-	}
-	t.Logf("\noutput:\n%s", string(b))
-}
 
 func TestGetTypeName(t *testing.T) {
 	cases := map[string]struct {
