fe956347ee66dd0c53eb03e21c32e56c2a8b267f
MizushimaToshihiko <pandachan5228@gmail.com>
2022-01-25 15:59:07 +0000

    add pointer tests

--- testdata/pointer.go
+++ testdata/pointer.go
@@ -25,22 +25,33 @@ func main() {
 	var y6 int = 5
 	*(&y6 - 2 + 1) = 7
 	assert(7, x6, "var x6 int=3; var y6 int=5; *(&y6-2+1)=7; x6")
-	// assert(5, ({ int x=3; (&x+2)-&x+3; }));
-	// assert(8, ({ int x, y; x=3; y=5; x+y; }));
-	// assert(8, ({ int x=3, y=5; x+y; }));
-
-	// assert(3, ({ int x[2]; int *y=&x; *y=3; *x; }));
-
-	// assert(3, ({ int x[3]; *x=3; *(x+1)=4; *(x+2)=5; *x; }));
-	// assert(4, ({ int x[3]; *x=3; *(x+1)=4; *(x+2)=5; *(x+1); }));
-	// assert(5, ({ int x[3]; *x=3; *(x+1)=4; *(x+2)=5; *(x+2); }));
-
-	// assert(0, ({ int x[2][3]; int *y=x; *y=0; **x; }));
-	// assert(1, ({ int x[2][3]; int *y=x; *(y+1)=1; *(*x+1); }));
-	// assert(2, ({ int x[2][3]; int *y=x; *(y+2)=2; *(*x+2); }));
-	// assert(3, ({ int x[2][3]; int *y=x; *(y+3)=3; **(x+1); }));
-	// assert(4, ({ int x[2][3]; int *y=x; *(y+4)=4; *(*(x+1)+1); }));
-	// assert(5, ({ int x[2][3]; int *y=x; *(y+5)=5; *(*(x+1)+2); }));
+	var x7 int = 3
+	assert(5, (&x7+2)-&x7+3, "var x7 int=3; (&x+2)-&x+3")
+	var x8 [2]int
+	var y8 *int = &x8
+	*y8 = 3
+	assert(3, *x8, "var x8 [2]int; var y8 *int=&x8; *y8=3; *x8")
+	var x9 [3]int
+	*x9 = 3
+	*(x9 + 1) = 4
+	*(x9 + 2) = 5
+	assert(3, *x9, "var x9 [3]int; *x9=3; *(x9+1)=4; *(x9+2)=5; *x9")
+	assert(4, *(x9 + 1), "var x9 [3]int; *x9=3; *(x9+1)=4; *(x9+2)=5; *(x9+1)")
+	assert(5, *(x9 + 2), "var x9 [3]int; *x9=3; *(x9+1)=4; *(x9+2)=5; *(x9+2)")
+	var x10 [2][3]int
+	var y10 *int = x10
+	*y10 = 0
+	assert(0, **x10, "var x10 [2][3]int; var y10 *int=x10; *y10=0; **x10")
+	*(y10 + 1) = 1
+	assert(1, *(*x10 + 1), "var x10 [2][3]int; var y10 *int=x10; *(y10+1)=1; *(*x10+1)")
+	*(y10 + 2) = 2
+	assert(2, *(*x10 + 2), "var x10 [2][3]int; var y10 *int=x10; *(y10+2)=2; *(*x10+2)")
+	*(y10 + 3) = 3
+	assert(3, **(x10 + 1), "var x10 [2][3]int; var y10 *int=x10; *(y10+3)=3; **(x10+1)")
+	*(y10 + 4) = 4
+	assert(4, *(*(x10 + 1) + 1), "var x10 [2][3]int; var y10 *int=x10; *(y10+4)=4; *(*(x10+1)+1)")
+	*(y10 + 5) = 5
+	assert(5, *(*(x10 + 1) + 2), "var x10 [2][3]int; var y10 *int=x10; *(y10+5)=5; *(*(x10+1)+2)")
 
 	// assert(3, ({ int x[3]; *x=3; x[1]=4; x[2]=5; *x; }));
 	// assert(4, ({ int x[3]; *x=3; x[1]=4; x[2]=5; *(x+1); }));
