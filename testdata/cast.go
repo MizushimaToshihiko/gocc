4345f44fc9a3df877c9c929506769a01a8df6f55
MizushimaToshihiko <pandachan5228@gmail.com>
2022-01-28 22:49:53 +0000

    add test

--- testdata/cast.go
+++ testdata/cast.go
@@ -3,9 +3,9 @@ package test
 func main() {
 	assert(131585, int(8590066177), "int(8590066177)")
 	assert(513, int16(8590066177), "int16(8590066177)")
-	// assert(1, (char)8590066177);
-	// assert(1, (long)1);
-	// assert(0, (long)&*(int *)0);
+	assert(1, byte(8590066177), "byte(8590066177)")
+	assert(1, int64(1), "int64(1)")
+	assert(0, int64(&*(*int)(0)), "int64(&*(*int)(0))")
 	// assert(513, ({ int x=512; *(char *)&x=1; x; }));
 	// assert(5, ({ int x=5; long y=(long)&x; *(int*)y; }));
 
