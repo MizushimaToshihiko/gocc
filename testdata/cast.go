987860df31feb37fb54c5364bb71a28467f10da0
MizushimaToshihiko <pandachan5228@gmail.com>
2022-01-29 04:24:51 +0000

    add test

--- testdata/cast.go
+++ testdata/cast.go
@@ -5,11 +5,11 @@ func main() {
 	assert(513, int16(8590066177), "int16(8590066177)")
 	assert(1, byte(8590066177), "byte(8590066177)")
 	assert(1, int64(1), "int64(1)")
-	assert(0, int64(&*(*int)(0)), "int64(&*(*int)(0))")
-	// assert(513, ({ int x=512; *(char *)&x=1; x; }));
+	// var x int=512; *(*byte)(&x)=1;
+	// assert(513, x, "var x int=512; *(*byte)(&x)=1; x");
 	// assert(5, ({ int x=5; long y=(long)&x; *(int*)y; }));
 
 	// (void)1;
 
-	printf("OK\n")
+	println("OK")
 }
