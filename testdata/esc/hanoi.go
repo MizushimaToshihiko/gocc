9a5bd09d4b49c1683d0779f9aa3f1936689a088c
MizushimaToshihiko <pandachan5228@gmail.com>
2022-03-14 10:17:12 +0000

    add a test

--- testdata/esc/hanoi.go
+++ testdata/esc/hanoi.go
@@ -12,6 +12,17 @@ func Hanoi(n int, from string, work string, dest string) {
 	}
 }
 
+func Hanoi2(n int, from string, work string, dest string) {
+	if n >= 1 {
+		Hanoi2(n-1, from, dest, work)
+		println("%d: from %s to %s", n, from, dest)
+		Hanoi2(n-1, work, from, dest)
+	}
+}
+
 func main() {
+	println("Hanoi1:")
 	Hanoi(4, "A", "B", "C")
+	println("Hanoi2:")
+	Hanoi2(4, "A", "B", "C")
 }
