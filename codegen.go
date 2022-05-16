efb0fd7cbd00c69e0263e3546f7561a3612211d9
MizushimaToshihiko <pandachan5228@gmail.com>
2022-05-16 07:58:58 +0000

    Add a comment

--- codegen.go
+++ codegen.go
@@ -45,7 +45,10 @@ var argreg8 = []string{"%dil", "%sil", "%dl", "%cl", "%r8b", "%r9b"}
 var argreg16 = []string{"%di", "%si", "%dx", "%cx", "%r8w", "%r9w"}
 var argreg32 = []string{"%edi", "%esi", "%edx", "%ecx", "%r8d", "%r9d"}
 var argreg64 = []string{"%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"}
+
+// registers for return values
 var retreg64 = []string{"%r10", "%r11", "%r12", "%r13", "%r14", "%r15"}
+
 var curFnInGen *Obj
 
 func (c *codeWriter) push() {
