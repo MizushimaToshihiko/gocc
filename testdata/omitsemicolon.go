383b8b63dcb181e5d7bd3785c07e62902835bc81
MizushimaToshihiko <pandachan5228@gmail.com>
2022-07-21 06:48:22 +0000

    Fixed tests for omitting semicolons

--- testdata/omitsemicolon.go
+++ testdata/omitsemicolon.go
@@ -3,11 +3,13 @@ package test_omitsemicolon
 func assert(want int, act int, code string)
 func println(format ...string)
 
-func foo() { return }
-func bar() { return 1 }
+func foo()            { return }
+func bar() (int, int) { return 1, 2 }
 
 func main() {
 	foo()
-	assert(1, bar(), "bar()")
+	a01, b01 := bar()
+	assert(1, a01, "bar()")
+	assert(2, b01, "bar()")
 	println("OK")
 }
